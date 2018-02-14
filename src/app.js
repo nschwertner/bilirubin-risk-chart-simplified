569cb1de89e2c6f9b1f9d380fc84d0bc4d52a36a
Nikolai Schwertner <nikolai@interopion.com>
2018-02-13 22:19:13 -0500

    Detab

--- src/app.js
+++ src/app.js
@@ -5,27 +5,27 @@ let state = {
 }
 
 const risks = {
-	highRiskLowerLimit: (hours) => (
-		      0.0000000001087380116978890 * Math.pow(hours, 6)
-			- 0.0000000390241213926723000 * Math.pow(hours, 5)
-			+ 0.0000051614113948939000000 * Math.pow(hours, 4)
-			- 0.0002969267656958150000000 * Math.pow(hours, 3)
-			+ 0.0049045801308693600000000 * Math.pow(hours, 2)
-			+ 0.2770830724994080000000000 * hours + 3.0000000000000000000000000),
-	highIntermediateLowerLimit: (hours) => (
-		      0.0000000001117640940944670 * Math.pow(hours, 6)
-			- 0.0000000412521674888165000 * Math.pow(hours, 5)
-			+ 0.0000056604841945917500000 * Math.pow(hours, 4)
-			- 0.0003464807831541350000000 * Math.pow(hours, 3)
-			+ 0.0075934710390583900000000 * Math.pow(hours, 2)
-			+ 0.1810763744197170000000000 * hours + 2.5000000000000000000000000),
-	lowIntermediateLowerLimit: (hours) => (
-		    0.0000000000055158434836619 * Math.pow(hours, 6) -
-			0.0000000020974548879410700 * Math.pow(hours, 5) +
-			0.0000002627978699654140000 * Math.pow(hours, 4) -
-			0.0000054294662703569000000 * Math.pow(hours, 3) -
-			0.0018169823503626500000000 * Math.pow(hours, 2) +
-			0.2329924996556660000000000 * hours + 2.0000000000000000000000000),
+    highRiskLowerLimit: (hours) => (
+              0.0000000001087380116978890 * Math.pow(hours, 6)
+            - 0.0000000390241213926723000 * Math.pow(hours, 5)
+            + 0.0000051614113948939000000 * Math.pow(hours, 4)
+            - 0.0002969267656958150000000 * Math.pow(hours, 3)
+            + 0.0049045801308693600000000 * Math.pow(hours, 2)
+            + 0.2770830724994080000000000 * hours + 3.0000000000000000000000000),
+    highIntermediateLowerLimit: (hours) => (
+              0.0000000001117640940944670 * Math.pow(hours, 6)
+            - 0.0000000412521674888165000 * Math.pow(hours, 5)
+            + 0.0000056604841945917500000 * Math.pow(hours, 4)
+            - 0.0003464807831541350000000 * Math.pow(hours, 3)
+            + 0.0075934710390583900000000 * Math.pow(hours, 2)
+            + 0.1810763744197170000000000 * hours + 2.5000000000000000000000000),
+    lowIntermediateLowerLimit: (hours) => (
+            0.0000000000055158434836619 * Math.pow(hours, 6) -
+            0.0000000020974548879410700 * Math.pow(hours, 5) +
+            0.0000002627978699654140000 * Math.pow(hours, 4) -
+            0.0000054294662703569000000 * Math.pow(hours, 3) -
+            0.0018169823503626500000000 * Math.pow(hours, 2) +
+            0.2329924996556660000000000 * hours + 2.0000000000000000000000000),
 }
 
 const filter = {
@@ -33,44 +33,44 @@ const filter = {
         return (new XDate(date)).toString(format)
       },
     age: (date) => {
-		let yearNow = new Date().getYear()
-		let monthNow = new Date().getMonth()
-		let dateNow = new Date().getDate()
+        let yearNow = new Date().getYear()
+        let monthNow = new Date().getMonth()
+        let dateNow = new Date().getDate()
 
         let yearDob = new Date(date).getYear()
-		let monthDob = new Date(date).getMonth()
-		let dateDob = new Date(date).getDate()
+        let monthDob = new Date(date).getMonth()
+        let dateDob = new Date(date).getDate()
 
         let yearAge = yearNow - yearDob
-		let monthAge = null
-		let dateAge = null
+        let monthAge = null
+        let dateAge = null
 
-		if (monthNow >= monthDob) monthAge = monthNow - monthDob
-		else {
-			yearAge--
-			monthAge = 12 + monthNow - monthDob
-		}
+        if (monthNow >= monthDob) monthAge = monthNow - monthDob
+        else {
+            yearAge--
+            monthAge = 12 + monthNow - monthDob
+        }
 
-		if (dateNow >= dateDob) dateAge = dateNow - dateDob
-		else {
-			monthAge--
-			dateAge = 31 + dateNow - dateDob
-			if (monthAge < 0) {
-				monthAge = 11
-				yearAge--
-			}
-		}
+        if (dateNow >= dateDob) dateAge = dateNow - dateDob
+        else {
+            monthAge--
+            dateAge = 31 + dateNow - dateDob
+            if (monthAge < 0) {
+                monthAge = 11
+                yearAge--
+            }
+        }
 
         let hours = (new Date().getTime() - new Date(date).getTime()) / 36e5
         if (dateAge > 1) hours = hours/(24 * dateAge)
 
         if ( (yearAge > 0) && (monthAge > 0) && (dateAge > 0) ) return yearAge + "y " + monthAge + "m " + dateAge + "d"
-		else if ( (yearAge > 0) && (monthAge > 0) && (dateAge == 0) ) return yearAge + "y " + monthAge + "m"
-		else if ( (yearAge > 0) && (monthAge == 0) && (dateAge > 0) ) return yearAge + "y " + dateAge + "d"
-		else if ( (yearAge > 0) && (monthAge == 0) && (dateAge == 0) ) return yearAge + "y"
-		else if ( (yearAge == 0) && (monthAge > 0) && (dateAge > 0) ) return monthAge + "m " + dateAge + "d"
-		else if ( (yearAge == 0) && (monthAge > 0) && (dateAge == 0) ) return monthAge + "m"
-		else if ( (yearAge == 0) && (monthAge == 0) && (dateAge > 1) ) return dateAge + "d"
+        else if ( (yearAge > 0) && (monthAge > 0) && (dateAge == 0) ) return yearAge + "y " + monthAge + "m"
+        else if ( (yearAge > 0) && (monthAge == 0) && (dateAge > 0) ) return yearAge + "y " + dateAge + "d"
+        else if ( (yearAge > 0) && (monthAge == 0) && (dateAge == 0) ) return yearAge + "y"
+        else if ( (yearAge == 0) && (monthAge > 0) && (dateAge > 0) ) return monthAge + "m " + dateAge + "d"
+        else if ( (yearAge == 0) && (monthAge > 0) && (dateAge == 0) ) return monthAge + "m"
+        else if ( (yearAge == 0) && (monthAge == 0) && (dateAge > 1) ) return dateAge + "d"
         else if ( (yearAge == 0) && (monthAge == 0) && (dateAge > 0) ) return hours.toFixed(2) + "h"
         else return "Could not calculate age"
       },
